b53a414ba01b627d4a5e15e4062c42b38f90ce9f
신윤우 <118812638+sinkwl@users.noreply.github.com>
2026-04-10 15:38:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.title('나의 첫 웹서비스 만들기!!!')
 name = st.text_input('이름을 입력하세요: ')
 game = st.selectbox('좋아하는 음식을 선택하세요: ', ['롤','발로란트'])
 if st.button('인사말 생성') : 
-  st.write(f'{name}님! 당신이 좋아하는 음식은 {game}군요!')
+  st.write(f'{name}님! 당신이 좋아하는 게임은 {game}군요!')
                     
              
