98497ac733ea98604d289f0fe81121d3f05067bc
신윤우 <118812638+sinkwl@users.noreply.github.com>
2026-04-10 15:30:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import steramlit as st
 
 st.title('나의 첫 웹서비스 만들기!!!')
 name = st.text_input('이름을 입력하세요: ')
-menu = st.selectbox('좋아하는 음식을 선택하세요: ', ['떡볶이','치즈볼']
+menu = st.selectbox('좋아하는 음식을 선택하세요: ', ['떡볶이','치즈볼'])
 if st.button('인사말 생성') : 
   st.write(f'{name}님! 당신이 좋아하는 음식은 {menu}군요!')
                     
